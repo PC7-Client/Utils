2709e6d9347184c78dce52cbc1a35ddb58eff603
AceSilentKill <122637621+AceSilentKill@users.noreply.github.com>
2023-05-21 10:17:33 -0500

    Update exampleRequire.js

--- exampleRequire.js
+++ exampleRequire.js
@@ -1,7 +1,1 @@
-window.rick = new Audio('https://henryruss2.github.io/rickroll.mp3');
-rick.roll = rick.play;
-
-rick.roll();
-document.addEventListener('DOMContentLoaded', () => { 
-    window.speechSynthesis.speak(Object.assign(new SpeechSynthesisUtterance(), { text: 'This is your require, please check the userscript readme if you dont like the song' }));
-});
+window.rick=new Audio("https://henryruss2.github.io/rickroll.mp3"),rick.roll=rick.play,rick.roll(),document.addEventListener("DOMContentLoaded",(()=>{window.speechSynthesis.speak(Object.assign(new SpeechSynthesisUtterance,{text:"This is your require, please check the userscript readme if you dont like the song"}))}));
